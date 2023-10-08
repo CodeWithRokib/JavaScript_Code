ed1d88f56818d05e8ce40f5cbc10d406fe358d51
Rokibul Hasan Rokib <rokibulhasan018722@gmail.com>
2023-10-08 20:57:44 +0600

    Fuction practice 01, 02

---
+++ function_01.js
@@ -0,0 +1,4 @@
+function funcName() {
+    console.log("Hello, I am Function");
+}
+funcName();
---
+++ function_02.js
@@ -0,0 +1,5 @@
+function sumFunc(a,b){
+    let sum = a+b;
+    return sum;
+}
+console.log(sumFunc(3,4));
